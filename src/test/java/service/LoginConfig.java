beb3c994f7e5cbe0b5219c3f09e7e788c554132e
Katrin <123@123.com>
2021-12-16 19:46:43 +0300

    11

--- src/test/java/service/LoginConfig.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import org.aeonbits.owner.Config;
-
-@LoginConfig.Sources({"classpath:config/loginConfig.properties"})
-public interface LoginConfig extends Config {
-    String login();
-    String password();
-}
